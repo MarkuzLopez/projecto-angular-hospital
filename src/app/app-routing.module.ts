c2146450bf5f3e3c369bee51ba532a4255bff805
Mark02Lopez <mamorales@todoensubastas.com.mx>
2019-02-20 14:07:05 -0600

    Modulos, separar pages y shared asu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,34 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
+// import { DashboardComponent } from './pages/dashboard/dashboard.component';
+// import { ProgressComponent } from './pages/progress/progress.component';
+// import { Graficas1Component } from './pages/graficas1/graficas1.component';
+// import { PagesComponent } from './pages/pages.component';
 import { NoPageFoundComponent } from './shared/no-page-found/no-page-found.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: PagesComponent,
-    children: [
-      {path: 'dashboard', component: DashboardComponent },
-      {path: 'progress', component: ProgressComponent },
-      {path: 'graficas1', component: Graficas1Component },
-      {path: '', redirectTo: '/dashboard', pathMatch: 'full' }
-      /// * TODOs cuando no existe ninguna ruta, mandarlos a dasboard */
-    ]
-  },
+  // {
+  //   path: '',
+  //   component: PagesComponent,
+  //   children: [
+  //     {path: 'dashboard', component: DashboardComponent },
+  //     {path: 'progress', component: ProgressComponent },
+  //     {path: 'graficas1', component: Graficas1Component },
+  //     {path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+  //     /// * TODOs cuando no existe ninguna ruta, mandarlos a dasboard */
+  //   ]
+  // },
   {path: 'login', component: LoginComponent },
   {path: 'register', component: RegisterComponent },
   {path: '**', component: NoPageFoundComponent  }  // * cuando no esta definida la ruta, mandarlos a Pagina No Encontrada * //
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes, {useHash: true} )],
-  // ? el {useHash: true}, sirve para redireccionar automaticamente hacia el principal *
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const APP_ROUTES = RouterModule.forRoot( routes, { useHash: true } );
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-// TODO Rutas ///
-import { AppRoutingModule } from './app-routing.module';
+// *  Rutas ///
+import { APP_ROUTES } from './app-routing.module';
+
+// ? Modulos
+import { PagesModule } from './pages/pages.module';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NoPageFoundComponent } from './shared/no-page-found/no-page-found.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages.component';
+
+// import { DashboardComponent } from './pages/dashboard/dashboard.component';
+// import { Graficas1Component } from './pages/graficas1/graficas1.component';
+// import { ProgressComponent } from './pages/progress/progress.component';
+
+// import { NoPageFoundComponent } from './shared/no-page-found/no-page-found.component';
+// import { HeaderComponent } from './shared/header/header.component';
+// import { SidebarComponent } from './shared/sidebar/sidebar.component';
+// import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+// import { PagesComponent } from './pages/pages.component';
+
 import { RegisterComponent } from './register/register.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NoPageFoundComponent,
-    DashboardComponent,
-    Graficas1Component,
-    ProgressComponent,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
-    RegisterComponent
+    RegisterComponent,
+    // DashboardComponent,
+    // Graficas1Component,
+    // ProgressComponent,
+    // NoPageFoundComponent,
+    // HeaderComponent,
+    // SidebarComponent,
+    // BreadcrumbsComponent,
+    // PagesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
